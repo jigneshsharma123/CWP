c8685c56a3dbfc8ada31c383a269ef2926f370f4
jignesh sharma <jigneshsharma9868@gmail.com>
2023-11-11 11:50:30 +0530

    added the favicon icon to the project

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,9 +1,19 @@
 module.exports.profile = (req,res) => {
   res.render('user_profile');
 }
+//render the sigup page 
 module.exports.signup = (req,res) => {
-  return res.render('sign_up');
+  return res.render('sign_up', {
+    title : 'CWP | SIGN UP'
+  });
 };
+//render the signin page
 module.exports.signin = (req,res)=> {
-  return res.render('sign_in');
+  return res.render('sign_in', {
+    title : 'CWP | SIGN IN'
+  });
+}
+//get the signup data 
+module.exports.create = (req,res)=> {
+    
 }
